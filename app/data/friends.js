2079fb9be1a8ee20ef3209c22036e25f0bf7afeb
Stephanie Romanowski <stephanie.romanowski@vantage-cg.com>
2019-08-22 10:52:11 -0400

    adding updates

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+	{
+		name: "Ross",
+		photo: "https://pixel.nymag.com/imgs/fashion/daily/2018/10/24/24-ross-friends.w700.h467.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Rachel",
+		photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_33137025-926c-e1540297492156.jpg?quality=90&strip=all&zoom=1&resize=644%2C525&ssl=1",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Monica",
+		photo: "https://vignette.wikia.nocookie.net/friends/images/9/9e/Monica-geller-picture.jpg/revision/latest?cb=20110723171212",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Pheobe",
+		photo: "https://imgix.bustle.com/rehost/2016/9/13/8050f7a3-6653-4162-9a64-189ecabf5215.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Joey",
+		photo: "https://vignette.wikia.nocookie.net/p__/images/a/ac/Joey_with_a_red_background_.jpeg/revision/latest/scale-to-width-down/333?cb=20160330143537&path-prefix=protagonist",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+		name: "Chandler",
+		photo: "https://upload.wikimedia.org/wikipedia/en/6/6c/Matthew_Perry_as_Chandler_Bing.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    }
+   
+];
+
+module.exports = friends;
